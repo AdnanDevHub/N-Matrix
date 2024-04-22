317407eb6e8593bc17286107fcff53ca7123e900
Adnan <157080044+AdnanDevHub@users.noreply.github.com>
2024-04-22 11:51:30 +0200

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Menu
+    {
+        
+        private NMatrix matrix = new NMatrix();
+
+        public void Run()
+        {
+            int num;
+            do
+            {
+                num = GetMenuChoice();
+                switch (num)
+                {
+                    case 1:
+                        CreateMatrix();
+
+                        break;
+                    case 2:
+                        GetElement();
+                        break;
+                    case 3:
+                        AddMatrix();
+                        break;
+                    case 4:
+                        MultiplyMatrix();
+                        break;
+                    case 5:
+                        PrintMatrix();
+                        break;
+
+                    default:
+                        Console.WriteLine("Have a nice Day :-)");
+                        break;
+                }
+
+            } while (num != 0);
+
+        }
+
+
+
+
+        private void DisplayMenu()
+        {
+            Console.WriteLine("********************************");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Create Matrix");
+            Console.WriteLine("2. Get Element");
+            Console.WriteLine("3. Add Matrix");
+            Console.WriteLine("4. Multiply Matrix");
+            Console.WriteLine("5. Print Matrix");
+
+            Console.WriteLine("*********************************");
+
+        }
+        private int GetMenuChoice()
+        {
+            int choice;
+            do
+            {
+                DisplayMenu();
+                Console.WriteLine("Enter your choice");
+                try
+                {
+                    choice = int.Parse(Console.ReadLine()!);
+                }
+                catch (System.FormatException)
+                {
+                    choice = -1;
+                }
+            } while (choice < 0 || choice > 5);
+            return choice;
+        }
+        private void CreateMatrix()
+        {
+            try
+            {
+                Console.WriteLine("Enter size of matrix");
+                int size = int.Parse(Console.ReadLine()!);
+                matrix = new NMatrix(size);
+                matrix.AddValues();
+                Console.WriteLine("Matrix created successfully!");
+                Console.WriteLine($"{matrix.ToString()}");
+            }
+            catch (NegativeSizeException)
+            {
+                Console.WriteLine("Size must be greater than 0");
+            }
+
+
+        }
+        private void GetElement()
+        {
+
+            try
+            {
+                Console.Write("Enter the index i j :: ");
+                string[] input = (Console.ReadLine()!).Split(" ");
+                int i = int.Parse(input[0]);
+                int j = int.Parse(input[1]);
+                int element = matrix.Get(i, j);
+                Console.WriteLine($"Element at index {i} {j} is: {element}");
+            }
+            catch (OutofNMatrixException)
+            {
+                Console.WriteLine("Exception caught: Index outside of matrix");
+            }
+            catch (MatrixNotSetException)
+            {
+                Console.WriteLine("Exception caught: Set matrix first");
+            }
+        }
+        private void AddMatrix()
+        {
+            try
+            {
+                Console.WriteLine("Enter size for matrix 1");
+                int size1 = int.Parse(Console.ReadLine()!);
+                NMatrix matrix1 = new NMatrix(size1);
+                matrix1.AddValues();
+                Console.WriteLine("Matrix created successfully!");
+                Console.WriteLine("Enter size for matrix 2");
+                int size2 = int.Parse(Console.ReadLine()!);
+                NMatrix matrix2 = new NMatrix(size2);
+                matrix2.AddValues();
+                Console.WriteLine("Matrix created successfully!");
+
+                NMatrix result = NMatrix.Add(matrix1, matrix2);
+                Console.WriteLine("Added successfully!");
+                Console.WriteLine($"{result.ToString()}\n\n");
+            }
+            catch (NegativeSizeException)
+            {
+                Console.WriteLine("Size must be greater than 0");
+            }
+            catch (DimensionMismatchException)
+            {
+                Console.WriteLine("Exception caught: invalid sum");
+            }
+
+        }
+        private void MultiplyMatrix()
+        {
+            Console.WriteLine("Enter size for matrix 1");
+            int size1 = int.Parse(Console.ReadLine()!);
+            NMatrix matrix1 = new NMatrix(size1);
+            matrix1.AddValues();
+            Console.WriteLine($"{matrix1.ToString() }");
+            Console.WriteLine("Matrix created successfully!");
+            Console.WriteLine("Enter size for matrix 2");
+            int size2 = int.Parse(Console.ReadLine()!);
+            NMatrix matrix2 = new NMatrix(size2);
+            matrix2.AddValues();
+            Console.WriteLine($"{matrix2.ToString()}");
+            Console.WriteLine("Matrix created successfully!");
+
+            try
+            {
+                NMatrix result = NMatrix.Multiply(matrix1, matrix2);
+                Console.WriteLine("Multiplied successfully!");
+                Console.WriteLine($"{result.ToString()}");
+            }
+            catch (DimensionMismatchException)
+            {
+                Console.WriteLine("Exception caught: invalid multiplication");
+            }
+        }
+        private void PrintMatrix()
+        {
+            try
+            {
+                Console.WriteLine($"{matrix.ToString()}\n\n");
+
+            }
+            catch (MatrixNotSetException)
+            {
+                Console.WriteLine("Exception caught: Set Matrix first");
+            }
+        }
+    }
+   
+}
---
+++ NMatrix.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class DimensionMismatchException : Exception { };
+    public class OutofNMatrixException : Exception { };
+    public class NegativeSizeException : Exception { };
+    public class MatrixNotSetException : Exception { };
+
+    public class NMatrix
+    {
+        private List<int> matrix;
+        private int n;
+        private bool isSetMatrix = false;
+
+        public NMatrix() 
+        {
+            n = 4;
+            int length = n*n;
+            matrix = new List<int>(length);
+            for (int i = 0; i < length; i++)
+            {
+                matrix.Add(0);
+            }
+
+
+        }
+        public NMatrix(int size)
+        { 
+           
+            isSetMatrix = true;
+            this.n = size;
+            int length = n * n;
+            matrix = new List<int>(length);
+            for(int i = 0; i < length; i++) 
+            {
+                matrix.Add(i+1);
+            }
+        }
+
+        private bool isNMatrix(int i, int j)
+        {
+            return i == j || j == 0 || j == n - 1;
+        }
+        public int Get(int i, int j)
+        {
+            if (!isSetMatrix)
+            {
+                throw new MatrixNotSetException();
+            }
+            if (i < 0 || i >= n || j < 0 || j >= n)
+            {
+                throw new OutofNMatrixException();
+            }
+            if (isNMatrix(i, j))
+            {
+                return matrix[i * n + j];
+            }
+            
+                return 0;
+           
+        }
+
+
+
+        public void Set(int i, int j, int value)
+        {
+            if (i < 0 || i >= n || j < 0 || j >= n)
+            {
+                throw new OutofNMatrixException();
+            }
+            
+            if (isNMatrix(i,j))
+            {
+                matrix[i * n + j] = value;
+            }
+            else if (value != 0)
+            {
+                throw new ArgumentException("Invalid position for nonzero value");
+            }
+        }
+
+        public void AddValues()
+        {
+            isSetMatrix = true;
+            Console.WriteLine("Enter values for the matrix:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (isNMatrix(i,j))
+                    {
+                        Console.Write($"Enter value for position [{i},{j}]: ");
+                        int value = Convert.ToInt32(Console.ReadLine());
+                        Set(i, j, value);
+                    }
+                }
+            }
+        }
+
+        
+        public static NMatrix Add(NMatrix m1, NMatrix m2)
+        {
+            if (m1.n != m2.n)
+            {
+                throw new DimensionMismatchException();
+            }
+            NMatrix sum = new NMatrix(m1.n);
+            for (int i = 0; i < m1.n; i++)
+            {
+                for (int j = 0; j < m1.n; j++)
+                {
+                    sum.Set(i, j, m1.Get(i, j) + m2.Get(i, j));
+                }
+            }
+
+            return sum;
+           
+            
+        }
+
+        public static NMatrix Multiply(NMatrix m1, NMatrix m2)
+        {
+            if (m1.n != m2.n)
+            {
+                throw new DimensionMismatchException();
+            }
+            NMatrix result = new NMatrix(m1.n);
+      
+            for (int i = 0; i < m1.n; i++)
+            {
+                for (int j = 0; j < m1.n; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < m1.n; k++)
+                    {
+                        sum += m1.Get(i, k) * m2.Get(k, j);
+                    }
+                    result.Set(i, j, sum);
+                }
+            }
+
+            return result;
+        
+
+    }
+
+        public override string ToString()
+        {
+            if (isSetMatrix == false)
+            {
+                throw new MatrixNotSetException();
+            }
+            string print = $"{n}x{n}\n";
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    print += Get(i, j).ToString().PadLeft(4);
+                }
+                print += "\n";
+            }
+            return print;
+        }
+        public override bool Equals(object obj)
+        {
+            var item = obj as NMatrix;
+            return item.n == n;
+        }
+    }
+}
